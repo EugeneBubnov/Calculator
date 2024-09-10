bff73d75285fd4c7528b767902ca58295479c823
EugeneKyoto <eugenebubnov@vk.com>
2024-09-10 16:51:05 +0300

    Add css

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 'use strict'
 
 let a = ''; //first number
-let b = ''; //second number
+let b = ''; //second number 
 let o = ''; //operator
 let finish = false;
 
 const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const operators = ['+', '-', '*', '/']
+const operators = ['+', '-', '*', '/'] //C, delete, secret-cat
