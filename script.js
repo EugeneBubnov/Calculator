418dc2dda5ce86c9b566223153932a04b3342751
EugeneKyoto <eugenebubnov@vk.com>
2024-09-12 14:47:56 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,54 +1,76 @@
-let a = ""; //first number
-let b = ""; //second number
-let o = ""; //operator
+let first = "";
+let second = "";
+let operator = "";
 let finish = false;
 
 const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-const operator = ["+", "-", "×", "÷", "%"]; // delete, secret-cat
+//const operators = ["+", "-", "×", "÷", "%"]; // delete, secret-cat
+const operators = {
+  Plus: "+",
+  Minus: "-",
+  Multi: "×",
+  Division: "÷",
+  Percent: "%",
+};
 
 //Display
 const out = document.querySelector(".calc-screen p");
 
 function clearAll() {
-  a = "";
-  b = "";
-  o = "";
+  first = "";
+  second = "";
+  operator = "";
   finish = false;
   out.textContent = 0;
 }
 
 document.querySelector(".btn.clear").onclick = () => {
-  console.log("0");
   clearAll();
 };
 
 document.querySelector(".btn.secret-cat").onclick = () => {
-  console.log("Cat!");
   out.textContent = "^о‿о^";
 };
 
 let charArray;
 let popped_char;
 document.querySelector(".btn.delete").onclick = () => {
-  if (b === "" && finish === false) {
-    charArray = a.split("");
+  if (second === "" && finish === false) {
+    charArray = first.split("");
     popped_char = charArray.pop();
-    a = charArray.join().toString().replaceAll(",", "");
-    out.textContent = a;
-  } else if (a !== "" && finish === false) {
-    charArray = b.split("");
+    first = charArray.join().toString().replaceAll(",", "");
+    out.textContent = first;
+  } else if (first !== "" && finish === false) {
+    charArray = second.split("");
     popped_char = charArray.pop();
-    b = charArray.join().toString().replaceAll(",", "");
-    out.textContent = b;
-  } else  { //TODO:сделать возможность удалять из результата(a)
-    charArray = a.split("");
+    second = charArray.join().toString().replaceAll(",", "");
+    out.textContent = second;
+  } else {
+    //TODO:сделать возможность удалять из результата(a)
+    charArray = first.split("");
     popped_char = charArray.pop();
-    a = charArray.join().toString().replaceAll(",", "");
+    first = charArray.join().toString().replaceAll(",", "");
   }
 };
 
+function sum(a, b) {
+  return a + b;
+}
+
+function minus(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
+
 function percentage(percent, total) {
-  return ((percent/ 100) * total).toFixed(2)
+  return ((percent / 100) * total).toFixed(2);
 }
 
 //Все действия происходят в классе buttons;
@@ -62,73 +84,69 @@ document.querySelector(".buttons").onclick = (event) => {
   //Если список диджит содержит значение нажатой кнопки, то выполняется след блок команд внутри if
   if (digit.includes(key)) {
     //Если 2 число пустое и оператор не выбран, заполняем 1 число
-    if (b === "" && o === "") {
-      a += key;
-      out.textContent = a;
-      console.log(a);
+    if (second === "" && operator === "") {
+      first += key;
+      out.textContent = first;
       /*
     Если оператор а и б заполнены и finish = true(результат получен), 
-    то а остаётся со значением предыдущего результата, а б принимает новое значение.cer
+    то а остаётся со значением предыдущего результата, а б принимает новое значение
     а финиш обновляется, т.е. ждёт получения нового результата для переменной а.
     */
-    } else if (a !== "" && b !== "" && finish) {
-      b = key;
+    } else if (first !== "" && second !== "" && finish) {
+      second = key;
       finish = false;
-      out.textContent = b;
-      console.log(b);
+      out.textContent = second;
     }
     // Ввод второго числа после выбора первого и нажатия по оператору
     else {
-      b += key;
-      out.textContent = b;
-      console.log(b);
+      second += key;
+      out.textContent = second;
     }
   }
   /*
   Если список операторов содержит значение выбранного оператора, то о примет это значение.
   return помогает выйти из блока выполнения условий.
   */
-  if (operator.includes(key)) {
-    o = key;
-    out.textContent = o;
+  if (Object.values(operators).includes(key)) {
+    operator = key;
+    out.textContent = operator;
     return;
   }
 
   //Если = уже нажали, то переменные преобразуются в number и в зависимости от оператора выполняется вычисление.
   if (key === "=") {
-    let num_a = Number(a);
-    let num_b = Number(b);
+    let num_a = Number(first);
+    let num_b = Number(second);
 
-    if (b === "") b = a;
+    if (second === "") second = first;
 
-    switch (o) {
-      case "+":
-        a = num_a + num_b;
+    switch (operator) {
+      case operators.Plus:
+        first = sum(num_a, num_b);
         break;
-      case "-":
-        a = num_a - num_b;
+      case operators.Minus:
+        first = minus(num_a, num_b);
         break;
-      case "×":
-        a = num_a * num_b;
+      case operators.Multi:
+        first = multiply(num_a, num_b);
         break;
-      case "÷":
+      case operators.Division:
         if (num_b === 0) {
           out.textContent = "ERROR";
-          a = "";
-          b = "";
-          o = "";
+          first = "";
+          second = "";
+          operator = "";
           return;
         }
-        a = num_a / num_b;
+        first = division(num_a, num_b);
         break;
-      case "%":
-        a = percentage(num_a, num_b);
+      case operators.Percent:
+        first = percentage(num_a, num_b);
         break;
     }
 
     finish = true;
-    a = String(a);
-    out.textContent = a;
-    console.log(a, b, o);
+    first = String(first);
+    out.textContent = first;
   }
 };
